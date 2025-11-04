e72026b7540a122bc729c9339a840ac6a14ff027
Anandhumca <chn25mca042@ceconline.edu>
2025-11-04 15:07:03 +0530

    Update pattern.py

--- anandhumca/pattern.py
+++ anandhumca/pattern.py
@@ -3,7 +3,7 @@ for i in range(1,step+1):
     for j in range(1,i+1):
         print("*",end="")
     print()
-for i in range(step-1,0,1):
+for i in range(step-1,0,-1):
     for j in range(1,i+1):
         print("*",end="")
     print()
